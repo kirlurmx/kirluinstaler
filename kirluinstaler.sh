da416f780ac381e0a0293bac353dd50a186cfb02
kirlurmx <49128740+kirlurmx@users.noreply.github.com>
2019-05-10 08:26:56 -0500

    Se han añadido más herramientas para descargar

--- kirluinstaler.sh
+++ kirluinstaler.sh
@@ -1,31 +1,35 @@
-#!/bin/bash
-      echo "created by kirlurmx"
-echo "hola futuros juackers rusos :y"
+
+n/bash
+                                                                                          echo "hola futuros juackers rusos :y"
+                               echo "create by kirlurmx"
 
 echo "[1] Shellphish
-[2] AIOChk 
+[2] AIOChk
 [3] PasTerm
-[4] Salir
+[4] mpsyt
+[5] Salir
 "
 read -p "opcion deseada:" variable
 case $variable in
-	1)     
-	    git clone https://github.com/thelinuxchoice/shellphish;mv shellphish /data/data/com.termux/files/home
 
-		;;
+        1)                                                                                            git clone https://github.com/thelinuxchoice/shellphish;mv shellphish /data/data/com.termux/files/home
 
- 	2) 
-		pkg install php -y;git clone https://github.com/Juni0r007/AIOChk.git;mv AIOChk /data/data/com.termux/files/home
-		;;
+                ;;
 
-	3)
-		pkg install php -y;git clone https://github.com/Juni0r007/PasTerm;mv PasTerm /data/data/com.termux/files/home
-		;;
+        2)                                                                                                pkg install php -y;git clone https://github.com/Juni0r007/AIOChk.git;mv AIOChk /data/data/com.termux/files/home
+                ;;
 
-	4)
-		exit
-		;;
-	*)
-		echo "opcion incorrecta,bye ;u";;
+        3)
+                pkg install php -y;git clone https://github.com/Juni0r007/PasTerm;mv PasTerm /data/data/com.termux/files/home
+                ;;
 
-	esac
+        4)  apt update && apt upgrade -y;apt install python -y;pip install mps-youtube;pip install youtube_dl;apt install mpv -y
+
+                ;;
+        5)  exit
+
+                ;;
+        *)
+                echo "opcion incorrecta,bye ;u";;
+
+        esac
